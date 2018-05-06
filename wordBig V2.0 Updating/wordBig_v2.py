4e49d366224e2675339c3962c5c44e6760a7744d
Sfsx <405909878@qq.com>
2018-05-06 15:34:02 +0800

    接边图幅

--- wordBig V2.0 Updating/wordBig_v2.py
+++ wordBig V2.0 Updating/wordBig_v2.py
@@ -5,8 +5,9 @@ import tkinter as tk
 import docx
 import xlrd
 import time
-import sys
+import os
 import threading
+import random
 from docx import Document
 from tkinter import ttk
 from docx.shared import Pt
@@ -14,7 +15,6 @@ from tkinter import filedialog
 from tkinter import messagebox
 from docxtpl import DocxTemplate
 from PIL import Image, ImageTk
-
 
 
 # 定义Application类表示应用/窗口，继承Frame类
@@ -34,7 +34,8 @@ class Application(tk.Frame):
         # self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         # 显示按钮，并使用grid布局
         # self.quitButton.grid()
-        load = Image.open(r'face.jpg')
+        print(os.path.dirname(__file__))
+        load = Image.open(os.path.dirname(__file__) + '/face.jpg')
         render = ImageTk.PhotoImage(load)
 
         img = tk.Label(image=render)
@@ -84,8 +85,8 @@ class Application(tk.Frame):
         self.btn.grid(row=6, column=4, columnspan=2)
 
         self.pbar = ttk.Progressbar(
-            orient = "horizontal", length = 110, mode = "determinate", value = 0)
-        self.pbar.grid(row=6, column=6,columnspan=2)
+            orient="horizontal", length=110, mode="determinate", value=0)
+        self.pbar.grid(row=6, column=6, columnspan=2)
         self.label4 = tk.Label(
             text="@联系作者：FJSM 汪含秋", fg="black", font=('微软雅黑', 7))
         self.label4.grid(row=7, column=4, columnspan=4)
@@ -106,10 +107,10 @@ class Application(tk.Frame):
         if save_path != '':
             messagebox.showinfo(title='提示', message="您选择的路径是：" + save_path)
             self.save_path = save_path
-     #       self.analysis_excel(self.excel_path)
+            #       self.analysis_excel(self.excel_path)
             self.pbar.start()
             self.thread1 = threading.Thread(
-                target = self.analysis_excel, args=(self.excel_path,))
+                target=self.analysis_excel, args=(self.excel_path, ))
             self.thread1.setDaemon(True)
             self.thread1.start()
         else:
@@ -118,28 +119,26 @@ class Application(tk.Frame):
     # 比例尺选择
     def select_ratio(self):
         selection = self.var.get()
-        scale = {
-            1: '1:500',
-            2: '1:1000',
-            3: '1:10000',
-            4: '1:50000'
-        }
+        scale = {1: '1:500', 2: '1:1000', 3: '1:10000', 4: '1:50000'}
         self.scale = scale.get(selection)
-     #   print(self.scale
+
     # 检查者+日期 格式转换
     def myfomart(self, head, tail, length):
         blank = ' '
-        return head + blank*(length - len(head)*2) + tail
-    #汉字 空格推算
+        return head + blank * (length - len(head) * 2) + tail
+
+    # 汉字 空格推算
     def myfomart2(self, head, length):
         blank = ' '
-        return head + blank*(length - len(head)*2)
-    #字母数字
+        return head + blank * (length - len(head) * 2)
+
+    # 字母数字
     def myfomart3(self, head, length):
         blank = ' '
-        return head + blank*(length - len(head))
-    #测区名称判定
-    #def fontJudge(self):
+        return head + blank * (length - len(head))
+
+    # 测区名称判定
+    # def fontJudge(self):
     #    end_font = self.entry.get()
     #    end_font.font.name = u'微软雅黑'
     #    end_font.font.size = Pt(10.5)
@@ -150,10 +149,10 @@ class Application(tk.Frame):
         # excel
         book = xlrd.open_workbook(excel_path)
         sheet1 = book.sheet_by_index(0)
-        print (sheet1.row_values(1))
-     #   print (sheet1.row_values(8))
+        # print (sheet1.row_values(8))
         sheet_row = sheet1.nrows
         sheet_col = sheet1.ncols
+        execl_data = []
         for r in range(sheet_row):
             if r == 0:
                 continue
@@ -163,25 +162,73 @@ class Application(tk.Frame):
                 if (isinstance(value, float)):
                     value = str(int(value))
                 data.append(value)
-         #   self.docxtpl_test(data)
+            execl_data.append(data)
+        for row in execl_data:
             self.thread2 = threading.Thread(
-                target=self.docxtpl_test,args=(data,)
-            )
+                target=self.docxtpl_test, args=(execl_data, row,))
             self.thread2.start()
-     #   print  ("结束")
+        # 测试用
+        # self.thread2 = threading.Thread(
+        #     target=self.docxtpl_test, args=(execl_data, execl_data[0],))
+        # self.thread2.start()
         return
-     #   messagebox.showinfo(title= '提示', message= '输出完成')
-
-
 
     # 将表格数据填入word
-    def docxtpl_test(self, data):
-     #   print(data)
-     #   print(len(data))
-        #newtime1 = self.myfomart2(data[24],12)
-        #new
+    def docxtpl_test(self, execl_data, data):
+        # 初始赋值
+        east = '自由图边。检查者：' + data[7]
+        south = '自由图边。检查者：' + data[7]
+        west = '自由图边。检查者：' + data[7]
+        north = '自由图边。检查者：' + data[7]
+        east_no = ''
+        south_no = ''
+        west_no = ''
+        north_no = ''
+        east_status = ''
+        south_status = ''
+        west_status = ''
+        north_status = ''
+        east_fix = ''
+        south_fix = ''
+        west_fix = ''
+        north_fix = ''
 
-        #old
+        # 生成目标图号
+        pictureNo = data[0]
+        head = pictureNo[0:4]
+        x = pictureNo[4:7]
+        y = pictureNo[7:]
+        eastNo = head + x + '%03d' % (int(y)+1)
+        westNo = head + x + '%03d' % (int(y)-1)
+        northNo = head + '%03d' % (int(x)-1) + y
+        southNo = head + '%03d' % (int(x)+1) + y
+
+        # 查找图号并构造数据
+        for row in execl_data:
+            if (row[0] == eastNo):
+                east_no = eastNo
+                east = '已与' + east_no + '图幅接边。 接边者：' + row[6] + '   检查者：' + row[6]
+                east_fix = '地物 0.1-1.0mm'
+                east_status = '地貌 2-5m' + (17-len(row[6])*2)*' ' + '接边者：' + row[6]
+            if (row[0] == northNo):
+                north_no = northNo
+                north = '已与' + north_no + '图幅接边。 接边者：' + data[6] + '   检查者：' + row[6]
+                north_fix = '地物 0.1-1.0mm'
+                north_status = '地貌 2-5m' + (17-len(data[6])*2)*' ' + '接边者：' + data[6]
+            if (row[0] == westNo):
+                west_no = westNo
+                west = '已与' + west_no + '图幅接边。 接边者：' + data[6] + '   检查者：' + row[6]
+                west_fix = '地物 0.1-1.0mm'
+                west_status = '地貌 2-5m' + (17-len(data[6])*2)*' ' + '接边者：' + data[6]
+            if (row[0] == southNo):
+                south_no = southNo
+                south = '已于' + south_no + '图幅接边。 接边者：' + row[6] + '   检查者：' + row[6]
+                south_fix = '地物 0.1-1.0mm'
+                south_status = '地貌 2-5m' + (17-len(row[6])*2)*' ' + '接边者：' + row[6]
+
+        # print(data)
+        # print(len(data))
+        # newtime1 = self.myfomart2(data[24],12)
         auth2 = self.myfomart(data[25], data[26], 16)
         check2 = self.myfomart(data[27], data[28], 16)
         auth3 = self.myfomart(data[29], data[30], 16)
@@ -197,21 +244,20 @@ class Application(tk.Frame):
         gs2 = self.myfomart3(data[45], 18)
         method3 = self.myfomart2(data[46], 16)
         gs3 = self.myfomart3(data[47], 18)
-     #   print (data[41])
-     #   print (len(data[41]))
-     #   print (soft)
-     #   print (data[42])
-     #   print (len(data[42]))
-     #   print (gs1)
-     #   print (data[43])
-     #   print (method2)
-     #   print (len(method2))
-     #   print (len(method3))
-     #   print (len(gs2))
-     #   print (len(gs3))
-        doc = DocxTemplate(r'blank_pack_v2.docx')
+        # print (data[41])
+        # print (len(data[41]))
+        # print (soft)
+        # print (data[42])
+        # print (len(data[42]))
+        # print (gs1)
+        # print (data[43])
+        # print (method2)
+        # print (len(method2))
+        # print (len(method3))
+        # print (len(gs2))
+        # print (len(gs3))
+        doc = DocxTemplate(os.path.dirname(__file__) + './blank_pack_v2.docx')
         context = {
-            #new add
             'gxmc1': data[48],
             'zynr1': data[49],
             'tbz1': data[50],
@@ -222,11 +268,10 @@ class Application(tk.Frame):
             'tbz2': data[55],
             'fzr2': data[56],
             'tbrq2': data[57],
-            #old
             '图号': data[0],
             '图名': data[1],
-     #       '测区名称': self.entry.get(),
-     #       '测区名称': variableName,
+            # '测区名称': self.entry.get(),
+            # '测区名称': variableName,
             '比例尺': self.scale,
             '年': data[2],
             'auth0': data[3],
@@ -266,34 +311,51 @@ class Application(tk.Frame):
             'gs2': gs2,
             'method3': method3,
             'gs3': gs3,
+            'east': east,
+            'south': south,
+            'west': west,
+            'north': north,
+            'east_no': east_no,
+            'south_no': south_no,
+            'west_no': west_no,
+            'north_no': north_no,
+            'east_status': east_status,
+            'south_status': south_status,
+            'west_status': west_status,
+            'north_status': north_status,
+            'east_fix': east_fix,
+            'north_fix': north_fix,
+            'south_fix': south_fix,
+            'west_fix': west_fix,
         }
         doc.render(context)
         test_path = self.save_path + "/" + data[0] + ".docx"
-     #   print (self.save_path)
+        # 测试用
+        # timestamp = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        # test_path = os.path.dirname(__file__) + "/" + timestamp + data[0] + ".docx"
         doc.save(test_path)
-     #  reread
-        document = docx.Document(test_path)
-        tables = document.tables
-        homeTable = tables[0]
-        homeTable.style.font.name = u'微软雅黑'
-        nameOfLocal = self.entry.get()
-        run = homeTable.cell(2, 0).paragraphs[0].add_run(nameOfLocal)
-        run.font.name = u'微软雅黑'
-        print (len(nameOfLocal))
-        if len(nameOfLocal) > 15:
-            run.font.size = Pt(10.5)
-        else:
-            run.font.size = Pt(15)
-        document.save(test_path)
-
+        # reread
+        # document = docx.Document(test_path)
+        # tables = document.tables
+        # homeTable = tables[0]
+        # homeTable.style.font.name = u'微软雅黑'
+        # nameOfLocal = self.entry.get()
+        # run = homeTable.cell(2, 0).paragraphs[0].add_run(nameOfLocal)
+        # run.font.name = u'微软雅黑'
+        # print(len(nameOfLocal))
+        # if len(nameOfLocal) > 15:
+        #     run.font.size = Pt(10.5)
+        # else:
+        #     run.font.size = Pt(15)
+        # document.save(test_path)
 
         self.pbar.stop()
-
         return
+
 
 def docx_test():
     # excel
-    book = xlrd.open_workbook(r'D:\工作\word操作\code_v2\code\data.xlsx')
+    book = xlrd.open_workbook(r'/data.xlsx')
     sheet1 = book.sheet_by_index(0)
     sheet_row = sheet1.nrows
     sheet_col = sheet1.ncols
@@ -306,9 +368,9 @@ def docx_test():
             if (isinstance(value, float)):
                 value = str(int(value))
             data.append(value)
-    #print(data)
+    # print(data)
     # word
-    document = docx.Document(r'D:/code/py_code/pyGUI/temple.docx')
+    document = docx.Document(r'/temple.docx')
     tables = document.tables
     homeTable = tables[0]
     # 图号
@@ -324,7 +386,7 @@ def docx_test():
     run.font.name = u'微软雅黑'
     run.font.size = Pt(15)
     # 比例尺
-    document = docx.Document(r'D:/code/py_code/pyGUI/temple.docx')
+    document = docx.Document(r'/temple.docx')
     tables = document.tables
     homeTable = tables[0]
     run = homeTable.cell(2, 0).paragraphs[0].add_run(data[2])
@@ -355,7 +417,7 @@ def docx_test():
     t = 0
     blank = ' '
     for p in paragraphs:
-     #   print(i)
+        #   print(i)
         i = i + 1
         if ('作业方法' in p.text):
             p.text = ''
@@ -394,12 +456,11 @@ def docx_test():
     test_path = "D:\工作\word操作\code_v2\code" + timestamp + ".docx"
     document.save(test_path)
 
-# docx_test()
 
 # 创建一个Application对象app
 app = Application()
 # 设置窗口标题为'First Tkinter'
 app.master.title = 'First Tkinter'
+app.analysis_excel(os.path.dirname(__file__) + '/data.xlsx')
 # 主循环开始
 app.mainloop()
-#messagebox.showinfo("ok")
